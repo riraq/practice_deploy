20a249f5e333cb6207c7b8a1f3943236726c073c
riraq <riraq@mail.usf.edu>
2021-05-25 16:11:27 -0500

    build files added

---
+++ client/build/precache-manifest.25a5a8bdc93dd2e03ede86d529258708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2faa1d28a61a91e96ab5aae3bd5192e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4007fdc5b0f1d420c858",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4007fdc5b0f1d420c858",
+    "url": "/static/js/main.1b8c5b0b.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
